6cfad83900c1b3a291d5025855e5dc9772f46916
murchie85 <murchie85@gmail.com>
2022-01-06 00:47:01 +0000

    minor

--- functions/search_gettr.py
+++ functions/search_gettr.py
@@ -1,17 +1,14 @@
 """
 gg is the client
 """
-searchTerm = 'Crypto'
-resultList = list(gg.search(searchTerm,max=50)) 
 
 def searchGG(gg,searchTerm, maxLim=50,printOut=False):
     searched = list(gg.search(searchTerm,max=maxLim))
-    sArray = []
     if(printOut):
-        for item in sArray:
+        for item in searched:
             for x in item:
                 print(str(x) + ': ' + str(item[x]))
             print('---')
             print('')
 
-    return(sArray)
+    return(searched)
